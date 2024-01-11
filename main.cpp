9fd904813c7cf136644ce902cf24a688ab2fa91b
alptekinkarabacak <alptekinkarabacak@gmail.com>
2024-01-11 21:11:39 +0300

    Variables_data_types md has been added

--- main.cpp
+++ main.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
-#include "limits"
+using namespace std;
 
-void increment(int &num) { // Referenced, so any changes in num also changes the original value
-    num++; // Modifies the 'num' parameter directly
-    std::cout<<"The address of num is: "<<&num<<"\n";
-    std::cout << "Inside function - Value of num: " << num << std::endl;
-}
+// create a class
+class Room {
+
+public:
+    double length;
+    double breadth;
+    double height;
+
+    [[nodiscard]] double calculateArea() const {
+        return length * breadth;
+    }
+
+    [[nodiscard]] double calculateVolume() const  {
+        return length * breadth * height;
+    }
+};
 
 int main() {
-    int value = 5;
-    std::cout<<"The address of value is: "<<&value<<"\n";
 
-    std::cout << "Before function call - Value: " << value << std::endl;
+    // create object of Room class
+    Room room1{1, 2, 3};
 
-    std::cout<<"The address of value is: "<<&value<<"\n";
+    // assign values to data members
+    room1.length = 42.5;
+    room1.breadth = 30.8;
+    room1.height = 19.2;
 
+    // calculate and display the area and volume of the room
+    cout << "Area of Room =  " << room1.calculateArea() << endl;
+    cout << "Volume of Room =  " << room1.calculateVolume() << endl;
 
-    increment(value); // Function call with 'value' as argument by reference
+    Room* room2 = new Room();
+    room2->length = 42.5;
+    room2->breadth = 30.8;
+    room2->height = 19.2;
 
-    std::cout << "After function call - Value: " << value << std::endl;
+    double x = room2->calculateArea();
+
+    // calculate and display the area and volume of the room
+    cout << "Area of the Second Room Room =  " << room2->calculateArea() << endl;
+    cout << "Volume of the Second Room =  " << room2->calculateVolume() << endl;
+
     return 0;
 }
-
